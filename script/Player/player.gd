b6f8a60a5f7156bb9c0e5f78e1484abb5e2aa58f
lucasfs7 <me@lucasfsouza.com.br>
2020-08-30 03:30:15 -0300

    playing with map generation constraints

--- script/Player/player.gd
+++ script/Player/player.gd
@@ -10,29 +10,29 @@ func _process(_delta):
 	if anin.animation == "New Anim":
 		mode = RigidBody2D.MODE_STATIC
 
-	
 
-func _input(event):		
+
+func _input(event):
 	if is_dead:
 		if event.is_action_pressed("Controlle"):
 			on_touch()
 	elif anin.animation == "New Anim":
 		mode = RigidBody2D.MODE_STATIC
-		
+
 	if event.is_action_pressed("shot"):
 		if !time.autostart:
 			create_shot()
 			restart_timer()
-		
+
 func on_touch():
 	mode = RigidBody2D.MODE_RIGID
-	apply_impulse(Vector2(0, 0), Vector2(0, -300)) 
-	
+	apply_impulse(Vector2(0, 0), Vector2(0, -300))
+
 func create_shot():
 	var shot = SHOT_SCENE.instance()
 	get_parent().add_child(shot)
 	shot.position = get_node("Position2D").global_position
-	
+
 func restart_timer():
 	time.wait_time = .5
 	time.autostart = true
