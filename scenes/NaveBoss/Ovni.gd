54f5a18515b115619a69eb0a6c4cf94ee3b0cd4b
lucasfs7 <me@lucasfsouza.com.br>
2020-08-31 11:57:51 -0300

    implement enemy death when shot by player

---
+++ scenes/NaveBoss/Ovni.gd
@@ -0,0 +1,44 @@
+extends KinematicBody2D
+
+
+export (int) var speed = 30
+
+var target
+var velocity = Vector2()
+var is_dead = false
+
+onready var sprite = $AnimatedSprite
+
+
+func _physics_process(_delta):
+	if target and not is_dead:
+		velocity = position.direction_to(target.position) * speed
+
+		if position.distance_to(target.position) > 5:
+			velocity = move_and_slide(velocity)
+
+
+func die():
+	is_dead = true
+	sprite.play("dead")
+
+
+func is_body_target(body) -> bool:
+	return body.name == "Player"
+
+
+func _on_FieldView_body_entered(body):
+	if is_body_target(body):
+		target = body
+
+
+func _on_FieldView_body_exited(body):
+	if is_body_target(body):
+		target = null
+
+
+func _on_DeathArea_body_entered(body):
+	if body.name == "Bullet":
+		die()
+		yield(get_tree().create_timer(0.5), "timeout")
+		queue_free()
--- script/shot_player/shot_player.gd
+++ script/shot_player/shot_player.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends RigidBody2D
 
 const SHOT_SPEED = 200
 var cena
@@ -14,20 +14,8 @@ func _process(delta):
 	var motion = Vector2(speed_x, speed_y) * SHOT_SPEED
 	position = position + motion * delta
 
+	if get_colliding_bodies().size() > 0:
+		queue_free()
+
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
-
-
-func _on_Area2D_body_entered(body):
-	if body.name == "boss_one":
-		cena.kill_Boss()
-	elif body.name == "boss_two":
-		cena.kill_Boss()
-	elif body.name == "boss_three":
-		cena.kill_Boss()
-	elif body.name == "boss_four":
-		cena.kill_Boss()
-	elif body.name == "boss_five":
-		cena.kill_Boss()
-	elif body.name == "boss_six":
-		cena.kill_Boss()
