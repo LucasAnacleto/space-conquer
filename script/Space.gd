a8286eb78788edff38600a7b2f1da71a6cfce185
lucas.la <lucas.la@deadbrain.games>
2020-09-11 14:21:23 -0300

    LaserBeam, score points

--- script/Space.gd
+++ script/Space.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+const SCORE_TEXT = "%sm"
 
 var platform_generation_chunk_size = 5
 var total_chunks_generated = 0
@@ -7,7 +8,7 @@ var total_chunks_generated = 0
 onready var player = $Player
 onready var platform_generator = $PlatformGenerator
 onready var map_chunk_width = int(get_viewport().size.x * platform_generation_chunk_size)
-
+onready var score = $UI/ControlScore/Score
 
 func _ready():
 	platform_generator.generate_map(map_chunk_width)
@@ -15,6 +16,7 @@ func _ready():
 
 
 func _process(_delta):	
+	process_game_flow()
 	var player_position = player.position
 	var viewport_size = get_viewport().size
 
@@ -28,10 +30,12 @@ func increment_total_chunks_generated() -> void:
 	total_chunks_generated += platform_generation_chunk_size
 
 
-func kill_Boss():
-	pass
-
 
 func _on_Player_player_died():
 	yield(get_tree().create_timer(3.0), "timeout")
 	get_tree().reload_current_scene()
+
+
+func process_game_flow():
+	if not player.is_dead and not get_tree().paused:
+		score.text = SCORE_TEXT % State.set_score(int(player.position.x) / 50)
--- script/State.gd
+++ script/State.gd
@@ -1,9 +1,18 @@
 extends Node
 
-
+var score := 0
+var total := 0
 var first_run := true
 
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	
+	
+func set_score(points: int) -> int:
+	if first_run:
+		first_run = false
+
+	score = points + total
+	return score
 
