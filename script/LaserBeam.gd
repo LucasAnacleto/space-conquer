1163e69dbfc86dd2b93c24caccf46a7c8b2d6d4c
Lucas F. Souza <lucas.fs@deadbrain.games>
2020-09-21 16:04:07 -0300

    fix player animations on mobile

--- script/LaserBeam.gd
+++ script/LaserBeam.gd
@@ -5,12 +5,12 @@ var is_casting := false setget set_is_casting
 onready var line = $Line2D
 onready var lasersom = $Audio/Firing
 
-	
+
 func _ready():
 	self.is_casting = false
 	line.visible = false
 	pass
-	
+
 #func _unhandled_input(event) -> void:
 #	if event is InputEventMouseButton:
 #		self.is_casting = event.pressed
@@ -18,7 +18,7 @@ func _ready():
 func _physics_process(_delta):
 	var cast_point := cast_to
 	force_raycast_update()
-	
+
 	$CollisionParticles2D.emitting = is_colliding()
 	if is_colliding():
 		cast_point = to_local(get_collision_point())
@@ -27,16 +27,16 @@ func _physics_process(_delta):
 		var body = get_collider()
 		if body.has_method("die"):
 			body.call("die")
-		
+
 	$Line2D.points[1] = cast_point
 	$BeamParticles2D.position = cast_point * 0.5
 	$BeamParticles2D.process_material.emission_box_extents.x = cast_point.length() * 0.5
-	
-	
-	
+
+
+
 func set_is_casting(cast: bool) -> void:
 	is_casting = cast
-	
+
 	$BeamParticles2D.emitting = is_casting
 	$CastingParticles2D.emitting = is_casting
 	if is_casting:
@@ -44,24 +44,26 @@ func set_is_casting(cast: bool) -> void:
 	else:
 		$CollisionParticles2D.emitting = false
 		disappaer()
-	
+
 	set_physics_process(is_casting)
-	
+
 func appear() -> void:
 	$Tween.stop_all()
 	$Tween.interpolate_property($Line2D, "width", 0, 10.0, 0.2)
 	$Tween.start()
-	
+
 func disappaer() -> void:
 	$Tween.stop_all()
 	$Tween.interpolate_property($Line2D, "width", 10.0, 0, 0.1)
 	$Tween.start()
 
-func _on_Player_player_laserbean(cena):
-	if cena.is_action("shot"):		
-		if cena.pressed:
-			if not self.is_casting:				
-				self.is_casting = cena.pressed
+func _on_Player_player_laserbean(input):
+	if input.is_action("shot"):
+		if input.pressed:
+			lasersom.play()
+
+			if not self.is_casting:
+				self.is_casting = input.pressed
 				line.visible = true
 		else:
 			self.is_casting = false
--- script/Player/Player.gd
+++ script/Player/Player.gd
@@ -4,57 +4,42 @@ extends RigidBody2D
 signal player_died
 signal player_laserbean
 
+var is_dead := false
+var impulse_offset := Vector2(0, 0)
+var impulse := Vector2(8, -80)
 
+onready var jump_sound := $Audio/Jump as AudioStreamPlayer
+onready var death_sound := $Audio/dead as AudioStreamPlayer
+onready var sprite := $AnimatedSprite as AnimatedSprite
+onready var helmet_light := $AnimatedSprite/HelmetLight as Light2D
+onready var laser := $LaserBeam2D as RayCast2D
+onready var laser_firing_sound := $LaserBeam2D/Audio/Firing as AudioStreamPlayer
 
-const SHOOT_COOLDOWN = 0.5
-
-var is_dead = false
-var can_shoot = true
-var impulse_offset = Vector2(0, 0)
-var impulse = Vector2(8, -80)
-
-onready var sprite = $AnimatedSprite
-onready var light = $AnimatedSprite/Light2D
-onready var laser = $LaserBeam2D
-onready var lase = $LaserBeam2D/Audio/Firing
 
 func _physics_process(_delta):
 	if get_colliding_bodies().size() > 0:
-		die()
+		if not is_dead:
+			sprite.play("dead")
+			death_sound.play()
+			is_dead = true
+			helmet_light.visible = false
 
+			emit_signal("player_died")
+
+	if not is_dead:
+		if Input.is_action_pressed("Controlle") or Input.is_action_pressed("shot"):
+			sprite.play('jump')
+		else:
+			sprite.play('idle')
 
 
 func _input(event):
 	if not is_dead:
 		if event.is_action_pressed("Controlle"):
-			jump()
-			$Audio/Jump.play()
-			$Audio/Jump.volume_db = -15
-		else:
-			sprite.play('idle')
+			jump_sound.play()
+			apply_impulse(impulse_offset, impulse)
 
-		var cena = event
 		if event.is_action_pressed("shot"):
-			emit_signal("player_laserbean", cena)
-			sprite.play('jump')
-			lase.playing = true
-			lase.volume_db = -10
+			emit_signal("player_laserbean", event)
 		else:
-			emit_signal("player_laserbean", cena)
-
-func jump():
-	sprite.play('jump')
-	apply_impulse(impulse_offset, impulse)
-
-
-func die():
-	if not is_dead:
-		$Audio/dead.play()
-		State.first_run = false
-		is_dead = true
-		mode = RigidBody2D.MODE_STATIC
-		light.visible = false
-		sprite.play("dead")
-
-	emit_signal("player_died")
-	laser.visible = false
+			emit_signal("player_laserbean", event)
